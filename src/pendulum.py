02c263b3cabc132f81d4d8c1a8f2488543454ae2
Harshvir Jaggi <harsh@Harsh-PC.localdomain>
2023-02-05 14:18:49 -0800

    Updates

--- src/pendulum.py
+++ src/pendulum.py
@@ -1,15 +1,14 @@
 """Pendulum."""
+from __future__ import annotations
+
+import random
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import torch
+import torch.nn as nn
+from torch.distributions.normal import Normal
+
 import gymnasium as gym
-
-env = gym.make("CartPole-v1", render_mode="human")
-observation, info = env.reset()
-
-for i in range(1000):
-    action = env.action_space.sample()  # agent policy that uses the observation and info
-    observation, reward, terminated, truncated, info = env.step(action)
-    print(f"Observation: {observation}    Reward: {reward]")
-
-    if terminated or truncated:
-        observation, info = env.reset()
-
-env.close()
